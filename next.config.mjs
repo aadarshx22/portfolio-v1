52414a866a185659b1e423290876f2da50df718c
Aadarsh <aadarshshekhar22@gmail.com>
2025-06-21 23:40:40 +0530

    added deploy workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+  basePath: '/your-repo-name', // IMPORTANT: replace with your GitHub repo name
 };
-
 export default nextConfig;
